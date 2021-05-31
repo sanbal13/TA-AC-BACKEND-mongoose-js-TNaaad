16157c42a495aa882da72bc5244b6ca8e38f892b
Santosh Balchandran <santoshbalchandran@gmail.com>
2021-05-31 12:05:40 +0530

    Explain with Code: Schema in Mongoose

---
+++ block-BNaacb/server/app.js
@@ -0,0 +1,34 @@
+let PORT = 4000;
+
+// requires
+let express = require('express');
+let logger = require('morgan');
+let mongoose = require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://localhost/sample',
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err) => {
+        console.log(err ? err : 'Connected to database');
+    }
+);
+
+// instantiate the app
+let app = express();
+
+// middlewares
+app.use(logger('dev'));
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+app.get('/users', (req, res) => {
+    res.send('Users Page');
+});
+
+
+// listener
+app.listen(PORT, () => {
+    console.log('server is listening on port ' + PORT);
+});
